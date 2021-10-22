15cd33ecc0181e58264a747abf94595eb5560a8c
pzawadzki <p.zawadzki1405>
2021-10-21 21:35:53 -0500

    added try except

--- usa_floor.py
+++ usa_floor.py
@@ -6,14 +6,15 @@ con = input('Which floor are you on?')
 
 try:
     res = int(con)
+    if (abc == 'USA') | (abc == 'Usa'):
+        res = res - 1
+        print('In Europe it will be floor nr', res)
+    elif abc == 'Europe':
+        res = res + 1
+        print('In USA it will be floor nr', res)
+    else: print('You have typed wrong country')
 except:
-    print('User has put word', res,'which is not a number')
+    print('User has put word', con,'which is not a number')
+    res = 0
 
 #abs
-if (abc == 'USA') | (abc == 'Usa'):
-    res = res - 1
-    print('In Europe it will be floor nr', res)
-elif abc == 'Europe':
-    res = res + 1
-    print('In USA it will be floor nr', res)
-else: print('You have typed wrong country')
