c39cc30dae15664c489fd992f43039671e7ce7b4
pzawadzki <p.zawadzki1405>
2021-10-26 21:51:31 -0500

    working with files2

--- files.py
+++ files.py
@@ -7,9 +7,10 @@ except:
     quit()                  #quit the program, skip the lines of code below
 
 count = 0
-for line in file:
+for line in file:   #line represent each line in code
     count=count+1
-    print(line.rstrip()[(line.find('[')+1):line.find(']')]) #rstrip skip \n and space
+    print(line.rstrip())
+    #print(line.rstrip()[(line.find('[')+1):line.find(']')]) #rstrip skip \n and space
                         #find show position of character in string
                         #character ':' cuts the string
 
